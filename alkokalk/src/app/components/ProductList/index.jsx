420b97e3e8693b04860e4f5c7512b628d36f7f3c
Jens Larsen <jenslarsenslottis@gmail.com>
2024-05-15 09:59:19 +0200

    Added APK sorter
    Added functionality to calculate the APK and sort it accordingly when creating the list.

--- alkokalk/src/app/components/ProductList/index.jsx
+++ alkokalk/src/app/components/ProductList/index.jsx
@@ -2,14 +2,19 @@ import { useEffect, useState } from 'react';
 import getSystembolagetData from "@/app/api/getSystembolagetData";
 
 const ProductList = () => {
-    const [data, setData] = useState(null);
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
-            const result = await getSystembolagetData();
-            if (result) {
-                console.log(result);
-                setData(result);
+            let data = await getSystembolagetData();
+            if (data) {
+                data = data.map(product => ({
+                    ...product,
+                    alcoholAmountMl: calculateAlcoholAmountMl(product.alcoholPercentage, product.volume),
+                    apk: calculateAPK(product.alcoholPercentage, product.volume, product.price)
+                })).sort((a, b) => b.apk - a.apk); 
+
+                setProducts(data);
             } else {
                 console.log('Failed to fetch data');
             }
@@ -18,15 +23,26 @@ const ProductList = () => {
         fetchData();
     }, []);
 
-    if (!data) {
+    const calculateAlcoholAmountMl = (alcoholPercentage, volume) => {
+        return (alcoholPercentage / 100) * volume;
+    };
+
+    const calculateAPK = (alcoholPercentage, volume, price) => {
+        const alcoholMl = calculateAlcoholAmountMl(alcoholPercentage, volume);
+        return (alcoholMl / price).toFixed(2);
+    };
+
+    if (!products.length) {
+        // Här kan vi returnera skeleton istället? //Jens
         return <div>Loading...</div>;
     }
 
     return (
         <div>
-            {data.map(product => (
-                <div key={product.productId}>
-                    {product.productNameBold}
+            {products.map((product, index) => (
+                <div key={index}>
+                    <div>Name: {product.productNameBold}</div>
+                    <div>APK: {product.apk} ml/kr</div> {}
                 </div>
             ))}
         </div>
