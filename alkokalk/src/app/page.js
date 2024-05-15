f54ba4158744deee8b7685bddb93626df4c27dfa
HPerssn <henrik.perssn@gmail.com>
2024-05-15 20:25:33 +0200

    got rid of sidescrolling and made som mobile adjustments

--- alkokalk/src/app/page.js
+++ alkokalk/src/app/page.js
@@ -1,11 +1,10 @@
-"use client"
-
+"use client";
 
 import NavigationBar from "./components/NavigationBar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <NavigationBar></NavigationBar>
     </main>
   );
