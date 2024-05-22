711d56ab80eb3d053b3039e1c8436e103ac097c5
HPerssn <henrik.perssn@gmail.com>
2024-05-22 09:19:05 +0200

    changed links to dynamic and made slight mobile styling

--- alkokalk/src/app/components/NavigationBar/index.jsx
+++ alkokalk/src/app/components/NavigationBar/index.jsx
@@ -1,29 +1,48 @@
-import { StyledNavigationBar, StyledLogoContainer, StyledNavLink, StyledLogoText, StyledNavLinkSection, LinkDivider } from "./styles";
+import {
+  StyledNavigationBar,
+  StyledLogoContainer,
+  StyledNavLink,
+  StyledLogoText,
+  StyledNavLinkSection,
+  LinkDivider,
+} from "./styles";
 import Image from "next/image";
+import Link from "next/link";
 
 const NavigationBar = () => {
-    return (
-        <StyledNavigationBar>
-            <StyledLogoContainer>
-                <Image 
-                    src="/images/alkokalk-logo.png" 
-                    alt="Alkokalk Logo" 
-                    width={180} 
-                    height={180}
-                />
-                <StyledLogoText>ALKOKALK</StyledLogoText>
-            </StyledLogoContainer>
-            <StyledNavLinkSection>
-                <LinkDivider></LinkDivider>
-                <StyledNavLink>Alkokalk</StyledNavLink>
-                <LinkDivider></LinkDivider>
-                <StyledNavLink>Om</StyledNavLink>
-                <LinkDivider></LinkDivider>
-                <StyledNavLink>Github</StyledNavLink>
-                <LinkDivider></LinkDivider>
-            </StyledNavLinkSection>
-        </StyledNavigationBar>
-    );
-}
+  const links = [
+    { title: "Alkokalk", href: "/home" },
+    { title: "Om", href: "/about" },
+    { title: "Github", href: "https://github.com/jenslars/Alkokalk" },
+  ];
+  return (
+    <StyledNavigationBar>
+      <StyledLogoContainer>
+        <Image
+          src="/images/alkokalk-logo.png"
+          alt="Alkokalk Logo"
+          width={180}
+          height={180}
+        />
+        <StyledLogoText>ALKOKALK</StyledLogoText>
+      </StyledLogoContainer>
+      <StyledNavLinkSection className="links">
+        <LinkDivider></LinkDivider>
+        {links.map((link) => (
+          <>
+            <Link
+              key={link.title}
+              href={link.href}
+              style={{ textDecoration: "none" }}
+            >
+              <StyledNavLink>{link.title}</StyledNavLink>
+            </Link>
+            <LinkDivider></LinkDivider>
+          </>
+        ))}
+      </StyledNavLinkSection>
+    </StyledNavigationBar>
+  );
+};
 
 export default NavigationBar;
--- alkokalk/src/app/components/NavigationBar/styles.js
+++ alkokalk/src/app/components/NavigationBar/styles.js
@@ -1,51 +1,83 @@
 import styled from "styled-components";
 
 export const StyledNavigationBar = styled.nav`
-    height: 70px;
-    color: #14242F;
-    display: flex;
+  height: 70%;
+  color: #14242f;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #fffcf0;
+  margin-top: 30px;
+  width: 100%;
+  padding: 0 20px;
+
+  @media (max-width: 768px) {
     align-items: center;
-    justify-content: space-between;
-    background-color: #FFFCF0;
-    margin-top: 30px;
-    width: 100%;
-    padding: 0 20px;
+    padding: 0;
+    margin-top: 0;
+    .links {
+      flex-direction: column;
+    }
+  }
 `;
 
 export const StyledLogoContainer = styled.div`
-    display: flex;
-    align-items: center;
-    font-size: 40px;
+  display: flex;
+  align-items: center;
+  font-size: 2.5rem;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
 `;
 
 export const StyledLogoText = styled.h1`
-    margin-left: 10px;
+  margin-left: 10px;
+
+    @media (max-width: 768px) {
+        margin-left: 0;
+        font-size: 1.5rem;
 `;
 
 export const StyledNavLinkSection = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    width: 500px;
-    margin-right: 5%;
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+  justify-content: flex-end;
+  width: 500px;
+  margin-right: 5%;
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-right: 0;
+    margin-left: 20%;
+  }
 `;
 
-export const StyledNavLink = styled.a`
-    color: #14242F;
-    text-decoration: none;
-    transition: 0.3s;
-    font-size: 22px;
+export const StyledNavLink = styled.span`
+  color: #14242f;
+  text-decoration: none;
+  transition: 0.3s;
+  font-size: 1.4rem;
 
-    &:hover {
-        font-weight: 500;
-        transition: 0.3s;
-    }
-    cursor: pointer;
+  &:hover {
+    font-weight: 500;
+    transition: 0.3s;
+  }
+  cursor: pointer;
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+  }
 `;
 
 export const LinkDivider = styled.div`
-    width: 1px;
-    height: 35px;
-    background-color: #828282;
-    margin: auto;
+  width: 1px;
+  height: 35px;
+  background-color: #828282;
+  margin: auto;
+  @media (max-width: 768px) {
+    width: 30%;
+    height: 1px;
+    margin: 5px 0;
+  }
 `;
